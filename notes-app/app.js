bcf5e40e0a280af0dd4345994aabb511e63f7084
Amit Kumar Das <amit.inverse@gmail.com>
2022-08-14 10:16:40 +0530

    notes-app 4

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,26 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 const notes = getNotes();
 console.log(notes);
 
-// console.log(validator.isEmail('amit@gmail.com'));
-// console.log(validator.isEmail('amitgmail.com'));
-// console.log(validator.isURL('https://teachyleaf.in'));
-console.log(validator.isURL('https:teachyleaf.in'));
+//
+// Challenge: Use the chalk library in your project
+//
+// 1. Install version 2.4.1 of chalk
+// 2. Load chalk into app.js
+// 3. Use it to print the string "Success" to the console in green
+// 4. Test your work
+//
+// Bonus: Use docs to mess around with other styles. Make text bold and inversed.
+
+console.log(chalk.green('Success'));
+console.log(chalk.green.bold('Success'));
+console.log(chalk.bold.green('Success'));
+console.log(chalk.bold.green.inverse('Success'));
+
+const textBold = chalk.bold;
+const textInversed = chalk.inverse;
+
+console.log(textBold('Amit Kumar Das'));
+console.log(textInversed('Amit Kumar Das'));
