7953eac4af361a15d08bf5696c5a944f815f5c89
Amit Kumar Das <amit.inverse@gmail.com>
2022-08-14 09:49:11 +0530

    notes-app 3

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,26 +1,10 @@
-// // const firstName = require('./utils.js');
-// const add = require('./utils.js');
-
-// // const name = 'Amit';
-
-// // console.log(name);
-
-// // console.log(firstName);
-
-// const sum = add(4, -2);
-
-// console.log(sum);
-
-//
-// Challenge: Define and use a function in an new file
-//
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From app.js, load in and call the function printing message to console
-
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const notes = getNotes();
-
 console.log(notes);
+
+// console.log(validator.isEmail('amit@gmail.com'));
+// console.log(validator.isEmail('amitgmail.com'));
+// console.log(validator.isURL('https://teachyleaf.in'));
+console.log(validator.isURL('https:teachyleaf.in'));
