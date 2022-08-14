53c9fe057d0b3d02fab57c228bb2ec2ef13582cf
Amit Kumar Das <amit.inverse@gmail.com>
2022-08-14 16:13:59 +0530

    notes-app 8

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,37 +5,50 @@ const getNotes = require('./notes.js');
 // customize yargs version
 yargs.version('1.1.0');
 
+//
+// Challenge: Add an option to yargs
+//
+// 1. Setup a body option for the add command
+// 2. Configure a description, make it required, and for it to be a string
+// 3. Log the body value in the handler function
+// 4. Test your work!
+
 // create add command
 yargs.command({
     command: 'add',
-    description: 'Add a new note',
-    handler: function () {
-        console.log('adding note');
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler: function (argv) {
+        // console.log('adding note', argv);
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
     },
 });
 
 // create remove command
 yargs.command({
     command: 'remove',
-    description: 'Remove a note',
+    describe: 'Remove a note',
     handler: function () {
         console.log('removing note');
     },
 });
 
-// add, remove, read, list
-
-//
-// Challenge: Add two new commands
-//
-// 1. Setup command to support "list" command (print placeholder messge for now)
-// 2. Setup command to support "read" command (print placeholder messge for now)
-// 3. Test your work by running both commands and ensure correct output
-
 // create list command
 yargs.command({
     command: 'list',
-    description: 'List all notes',
+    describe: 'List all notes',
     handler: function () {
         console.log('listing notes');
     },
@@ -44,11 +57,10 @@ yargs.command({
 // create read command
 yargs.command({
     command: 'read',
-    description: 'Reading a note',
+    describe: 'Reading a note',
     handler: function () {
         console.log('reading note');
     },
 });
 
-// console.log(process.argv);
-console.log(yargs.argv);
+yargs.argv;
