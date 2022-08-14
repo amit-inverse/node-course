568c580cc78c70c3e63affccacc826340942b1ff
Amit Kumar Das <amit.inverse@gmail.com>
2022-08-14 11:20:53 +0530

    notes-app 6

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,26 +1,18 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-const notes = getNotes();
-console.log(notes);
+// const notes = getNotes();
+// console.log(notes);
 
-//
-// Challenge: Use the chalk library in your project
-//
-// 1. Install version 2.4.1 of chalk
-// 2. Load chalk into app.js
-// 3. Use it to print the string "Success" to the console in green
-// 4. Test your work
-//
-// Bonus: Use docs to mess around with other styles. Make text bold and inversed.
+// console.log(chalk.green('Success'));
 
-console.log(chalk.green('Success'));
-console.log(chalk.green.bold('Success'));
-console.log(chalk.bold.green('Success'));
-console.log(chalk.bold.blue.inverse('Success'));
+console.log(process.argv);
+// console.log(process.argv[2]);
 
-const textBold = chalk.bold;
-const textInversed = chalk.inverse;
+const command = process.argv[2];
 
-console.log(textBold('Amit Kumar Das'));
-console.log(textInversed('Amit Kumar Das'));
+if (command === 'add') {
+    console.log('Adding note...');
+} else if (command === 'remove') {
+    console.log('Removing note...');
+}
