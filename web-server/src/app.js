4b20a20037e92b0596332a98fd9432127683b969
Amit Kumar Das <amit.inverse@gmail.com>
2022-08-15 20:35:48 +0530

    weather-app 14

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,31 +2,39 @@ const express = require('express');
 
 const app = express();
 
-// app.com
-// app.com/help
-// app.com/about
-
 app.get('', (req, res) => {
-    res.send('hello express!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('help page');
+    res.send([
+        {
+            name: 'Amit',
+            age: 32,
+        },
+        {
+            name: 'Rebecca',
+        },
+    ]);
 });
 
 //
-// Goal: Setup two new routes
+// Goal: Update routes
 //
-// 1. Setup an about route and render a page title
-// 2. Setup a weather route and render a page title
+// 1. SetUP about route to render a title with HTML
+// 2. Setup weather route to send back JSON
+//  - Object with forecast and location string
 // 3. Test your work by visiting both in the browser
 
 app.get('/about', (req, res) => {
-    res.send('about page');
+    res.send('<h1>about page</h1>');
 });
 
 app.get('/weather', (req, res) => {
-    res.send('weather page');
+    res.send({
+        forcast: 'Sunny',
+        location: 'Kolkata',
+    });
 });
 
 app.listen(3000, () => {
