7f31d43e5aa99774fe1f82180e53c011bea12212
Amit Kumar Das <amit.inverse@gmail.com>
2022-08-15 15:48:25 +0530

    weather-app 7

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const geocode = require('./utils/geocode');
 
 // const url = 'http://api.weatherstack.com/current?access_key=14e9343be42b0248fa9dff9864edf870&query=22.5726,88.3639&units=s';
 
@@ -16,27 +17,21 @@ const request = require('request');
 //     }
 // });
 
-// Geocoding
-// Address => lat/long => weather
+// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGVhY2h5bGVhZiIsImEiOiJja3JkYnp1bjU0dWU2Mm9ueGI0dWc1Y2piIn0.uAMoTQst3fo5DLGb6xrOIA&limit=1';
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to location services.');
+//     } else if (response.body.features.length === 0) {
+//         console.log('Unable to find location.');
+//     } else {
+//         const latitude = response.body.features[0].geometry.coordinates[1];
+//         const longitude = response.body.features[0].geometry.coordinates[0];
 
-//
-// Goal: Handle errors for geocoding reqeust
-//
-// 1. Setup an error handler for low-level errors
-// 2. Test by disabling network request and running the app
-// 3. Setup error handling for no matching result
-// 4. Test by altering the search term and running the app
+//         console.log('Latitude : ' + latitude + ', Longitude : ' + longitude);
+//     }
+// });
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGVhY2h5bGVhZiIsImEiOiJja3JkYnp1bjU0dWU2Mm9ueGI0dWc1Y2piIn0.uAMoTQst3fo5DLGb6xrOIA&limit=1';
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location services.');
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location.');
-    } else {
-        const latitude = response.body.features[0].geometry.coordinates[1];
-        const longitude = response.body.features[0].geometry.coordinates[0];
-
-        console.log('Latitude : ' + latitude + ', Longitude : ' + longitude);
-    }
+geocode('Kolkata', (error, data) => {
+    console.log('Error: ', error);
+    console.log('Data: ', data);
 });
