fb55184656c82018ee9976aebd586200f19e0355
Amit Kumar Das <amit.inverse@gmail.com>
2022-08-15 17:27:22 +0530

    weather-app 10

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,45 @@
+// Object property shorthand
+
+const name = 'Amit';
+const userAge = 32;
+
+// const user = {
+//     name: name,
+//     age: userAge,
+//     location: 'Kolkata',
+// };
+
+const user = {
+    name,
+    age: userAge,
+    location: 'Kolkata',
+};
+
+console.log(user);
+
+// Object destructuring
+
+const product = {
+    label: 'Red Notebook',
+    price: 3,
+    stock: 332,
+    salePrice: undefined,
+};
+
+// const label = product.label;
+// const stock = product.stock;
+
+// const { label: productLabel, stock = 100, rating = 5 } = product;
+// console.log(productLabel);
+// console.log(stock);
+// console.log(rating);
+
+// const transaction = (type, myProduct) => {
+//     const { label } = myProduct;
+// };
+
+const transaction = (type, { label, stock }) => {
+    console.log(type, label, stock);
+};
+
+transaction('order', product);
