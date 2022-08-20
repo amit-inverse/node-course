1edf9c7c8516724ede075c9d4c548705f38119d4
Amit Kumar Das <amit.inverse@gmail.com>
2022-08-20 17:40:31 +0530

    weather-app 26

--- web-server/public/js/script.js
+++ web-server/public/js/script.js
@@ -1,27 +1,29 @@
 console.log('Client-side JS loaded...');
 
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     });
-// });
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+    // console.log(location);
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        });
+    });
+});
 
 //
-// Goal: Fetch weather!
+// Goal: Use input value to get weather
 //
-// 1. Setup a call to fetch weather from Kolkata
-// 2. Get the parse JSON response
-//  - If error property, print error
-//  - If no error property, print weather and forecast
-// 3. Refresh the browser and test your work
-
-fetch('http://localhost:3000/weather?address=kolkata').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-    })
-})
+// 1. Migrate fetch call into the submit callback
+// 2. Use the search text as teh address query string value
+// 3. Submit the form with a valid and invalid value to test
