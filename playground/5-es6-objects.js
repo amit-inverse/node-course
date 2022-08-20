7f76bf08d729b915c9a624fa9a7029ab0624d628
Amit Kumar Das <amit.inverse@gmail.com>
2022-08-20 10:54:03 +0530

    weather-app 24

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -38,8 +38,9 @@ const product = {
 //     const { label } = myProduct;
 // };
 
-const transaction = (type, { label, stock }) => {
+const transaction = (type, { label, stock = 0 } = {}) => {
     console.log(type, label, stock);
 };
 
 transaction('order', product);
+transaction('order');
